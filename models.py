c400ce0f33f9e4a41d0138896f0a95c59356e83d
Ekevoo <x@ekevoo.com>
2015-04-13 18:12:34 -0300

    lots and lots of changes, i don't even remember

--- models.py
+++ models.py
@@ -10,26 +10,38 @@ class Event(models.Model):
 	price = PriceField()
 	capacity = models.IntegerField()
 	subs_open = models.BooleanField(default=False)
-	subs_start_at = models.DateTimeField(null=True)
+	subs_start_at = models.DateTimeField(null=True, blank=True)
 	sales_open = models.BooleanField(default=False)
-	sales_start_at = models.DateTimeField(null=True)
+	sales_start_at = models.DateTimeField(null=True, blank=True)
 	def __str__(self): return self.name
 
 class Optional(models.Model):
 	event = models.ForeignKey(Event)
 	name = models.CharField(max_length=20)
 	price = PriceField()
-	capacity = models.IntegerField(null=True)
+	capacity = models.IntegerField(null=True, blank=True)
 	def __str__(self): return self.name
 
 class Subscription(models.Model):
 	event = models.ForeignKey(Event)
 	user = models.ForeignKey(User, null=True)
-	badge = models.CharField(max_length=30)
 	created_at = models.DateTimeField()
-	position = models.IntegerField(null=True)
+	full_name = models.CharField(max_length=80)
+	document = models.CharField(max_length=30)
+	badge = models.CharField(max_length=30)
+	email = models.CharField(max_length=80)
+	area = models.CharField(max_length=2)
+	phone = models.CharField(max_length=9)
+	born = models.DateField()
+	shirt_size = models.CharField(max_length=4)
+	blood = models.CharField(max_length=3)
+	health_insured = models.BooleanField(default=False)
+	contact = models.TextField()
+	medication = models.TextField()
+	agreed = models.BooleanField(default=False)
+	position = models.IntegerField(null=True, blank=True)
 	paid = models.BooleanField(default=False)
-	paid_at = models.DateTimeField(null=True)
+	paid_at = models.DateTimeField(null=True, blank=True)
 	def __str__(self): return self.badge
 
 class Opted(models.Model):
@@ -49,7 +61,7 @@ class Transaction(models.Model):
 	subscription = models.ForeignKey(Subscription)
 	payee = models.CharField(max_length=10)
 	started_at = models.DateTimeField()
-	ended_at = models.DateTimeField(null=True)
+	ended_at = models.DateTimeField(null=True, blank=True)
 	accepted = models.BooleanField(default=False)
 	value = PriceField()
 	method = models.CharField(max_length=1, choices=METHODS, default=CASH)
--- views.py
+++ views.py
@@ -1,17 +1,46 @@
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.template import RequestContext, loader
+from django.utils import timezone
+from .models import Event, Subscription
+
+def get_event():
+	"""Takes first Event set in the future."""
+	queryset = Event.objects
+	queryset = queryset.filter(starts_at__gt = timezone.now())
+	queryset = queryset.order_by('starts_at')
+	return queryset.first()
+
+def get_subscription(user):
+	"""Takes existing subscription if available, creates a new one otherwise."""
+	event = get_event()
+	queryset = Subscription.objects
+	queryset = queryset.filter(event = event, user = user)
+	result = queryset.first()
+	if result is None:
+		result = Subscription(event = event, user = user)
+	return result
 
 def index(request):
 	return edit(request)
 
 def edit(request):
-	template = loader.get_template('inev/edit.html')
-	context = RequestContext(request, {
+	context = {
 		'errors': {},
-		'inputs': {},
-		'event': {},
+		'inputs': get_subscription(request.user),
+		'event': get_event(),
 		'optionals': [],
-	})
-	return HttpResponse(template.render(context))
+	}
+	return render(request, 'inev/edit.html', context)
 
+def view(request):
+	context = {
+		'inputs': {},
+		'event': get_event(),
+		'optionals': [],
+	}
+	return render(request, 'inev/view.html', context)
+
+edit = login_required(edit)
+view = login_required(view)
