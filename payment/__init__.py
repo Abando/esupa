b9a2f59440ca6b6e162da96f5a11f9d92d92759a
Ekevoo <x@ekevoo.com>
2015-05-15 18:22:24 -0300

    Split Deposit submodule.

--- payment/__init__.py
+++ payment/__init__.py
@@ -4,9 +4,9 @@ from logging import getLogger
 from django.conf import settings
 from django.core.exceptions import PermissionDenied
 from django.http import HttpResponseBadRequest
-from django.utils.timezone import now
 
-from ..models import PmtMethod, Subscription, Transaction
+from ..models import PmtMethod
+from .deposit import Deposit
 
 logger = getLogger(__name__)
 
@@ -50,37 +50,3 @@ class Processor:
 
     def generate_transaction_url(self):
         raise NotImplementedError()
-
-
-class Deposit:
-    def __init__(self, subscription):
-        if not subscription.id:
-            raise PermissionDenied('Payment without subscription.')
-        assert isinstance(subscription, Subscription)
-        self.subscription = subscription
-        self.slot_qs = subscription.transaction_set.filter(method=PmtMethod.DEPOSIT, filled_at__isnull=True)
-
-    @property
-    def expecting_file(self):
-        return self.slot_qs.exists()
-
-    def got_file(self, file):
-        transaction = self._get_or_create_transaction()
-        transaction.document = file.read()
-        transaction.filled_at = now()
-        transaction.save()
-
-    def register_intent(self):
-        transaction = self._get_or_create_transaction()
-        if not transaction.id:
-            transaction.save()
-
-    def _get_or_create_transaction(self) -> Transaction:
-        return self.slot_qs.first() or Transaction(subscription=self.subscription, value=self.subscription.price,
-                                                   method=PmtMethod.DEPOSIT)
-
-    def accept(self, acceptable):
-        transaction = self.subscription.transaction_set.get(method=PmtMethod.DEPOSIT, ended_at__isnull=True)
-        transaction.accepted = acceptable
-        transaction.ended_at = now()
-        transaction.save()
---
+++ payment/deposit.py
@@ -0,0 +1,43 @@
+# coding=utf-8
+from logging import getLogger
+
+from django.core.exceptions import PermissionDenied
+from django.utils.timezone import now
+
+from ..models import PmtMethod, Subscription, Transaction
+
+logger = getLogger(__name__)
+
+
+class Deposit:
+    def __init__(self, subscription):
+        if not subscription.id:
+            raise PermissionDenied('Payment without subscription.')
+        assert isinstance(subscription, Subscription)
+        self.subscription = subscription
+        self.slot_qs = subscription.transaction_set.filter(method=PmtMethod.DEPOSIT, filled_at__isnull=True)
+
+    @property
+    def expecting_file(self):
+        return self.slot_qs.exists()
+
+    def got_file(self, file):
+        transaction = self._get_or_create_transaction()
+        transaction.document = file.read()
+        transaction.filled_at = now()
+        transaction.save()
+
+    def register_intent(self):
+        transaction = self._get_or_create_transaction()
+        if not transaction.id:
+            transaction.save()
+
+    def _get_or_create_transaction(self) -> Transaction:
+        return self.slot_qs.first() or Transaction(subscription=self.subscription, value=self.subscription.price,
+                                                   method=PmtMethod.DEPOSIT)
+
+    def accept(self, acceptable):
+        transaction = self.subscription.transaction_set.get(method=PmtMethod.DEPOSIT, ended_at__isnull=True)
+        transaction.accepted = acceptable
+        transaction.ended_at = now()
+        transaction.save()
