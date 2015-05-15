b5dbac7b9da1df8859447bac8f264e9ecba46a94
Ekevoo <x@ekevoo.com>
2015-05-14 22:49:09 -0300

    added default routing

--- urls.py
+++ urls.py
@@ -2,11 +2,11 @@
 from django.conf.urls import url
 
 urlpatterns = [
-    url(r'^subscribe$', 'esupa.views.view_subscribe', name='esupa-subscribe'),
-    url(r'^subscribe/(.*)$', 'esupa.views.view_subscribe', name='esupa-subscribe'),
+    url(r'^$', 'esupa.views.view_subscribe', name='esupa-subscribe'),
     url(r'^verify$', 'esupa.views.view_verify', name='esupa-verify'),
     url(r'^verify/(\d+)$', 'esupa.views.view_verify_event', name='esupa-verify-event'),
     url(r'^doc/(.+)$', 'esupa.views.view_transaction_document', name='esupa-trans-doc'),
     url(r'^cron/(.+)$', 'esupa.views.view_cron', name='esupa-cron'),
     url(r'^processor/(.+)', 'esupa.views.view_processor', name='esupa-processor'),
+    url(r'^(.*)$', 'esupa.views.view_subscribe', name='esupa-subscribe'),
 ]
