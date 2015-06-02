ba6b32734f3ba329ea5dbaf936aa1bce210c0d90
Ekevoo <x@ekevoo.com>
2015-06-02 13:52:47 -0300

    Updated copyright name.

--- esupa/urls.py
+++ esupa/urls.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 #
-# Copyright 2015, Abando.
+# Copyright 2015, Abando.com.br
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in
 # compliance with the License. You may obtain a copy of the License at
